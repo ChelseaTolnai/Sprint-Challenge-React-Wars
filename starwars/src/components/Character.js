7ee6aae885fd7a33778be01ff2010eced00c6429
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-01-11 16:53:41 -0500

    Added further Character information and begin working on possible backside of card.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CharacterFilms from './Character';
 import './StarWars.css';
 
 const Character = (props) => {
@@ -10,25 +11,81 @@ const Character = (props) => {
             return res.json();
             })
             .then (data => {
-                document.getElementById(`${props.char.name}`).innerHTML = data.name
+                document.getElementById(`${props.char.name}-1`).innerHTML = data.name
             })
             .catch(err => {
             throw new Error(err);
             }); 
     };
 
+    function getLang(URL) {
+
+        fetch(URL)
+            .then(res => {
+            return res.json();
+            })
+            .then (data => {
+                document.getElementById(`${props.char.name}-2`).innerHTML = data.language
+            })
+            .catch(err => {
+            throw new Error(err);
+            }); 
+    };
+
+    function getHomeWorld(URL) {
+
+        fetch(URL)
+            .then(res => {
+            return res.json();
+            })
+            .then (data => {
+                document.getElementById(`${props.char.name}-3`).innerHTML = data.name
+            })
+            .catch(err => {
+            throw new Error(err);
+            }); 
+    };
+
+    // function getFilm(URL) {
+
+    //     fetch(URL)
+    //         .then(res => {
+    //         return res.json();
+    //         })
+    //         .then (data => {
+    //             document.querySelectorAll(`#${URL}`).innerHTML = data.title
+    //         })
+    //         .catch(err => {
+    //         throw new Error(err);
+    //         }); 
+    // };
+
     return (
         <div className="char-card">
             <div className="char-img"></div>
             <div className="char-head">
                 <h3 className="char-name">{props.char.name}</h3>
-                <p className="char-species" id={props.char.name}>{getSpecies(`${props.char.species[0]}`)}</p>
+                <p id={`${props.char.name}-1`}>{getSpecies(`${props.char.species[0]}`)}</p>
             </div>
             <ul className="char-list"> 
                 <li>Gender: {props.char.gender}</li>
                 <li>Height: {props.char.height}</li>
                 <li>Mass: {props.char.mass}</li>
+                <li>Home: <span id={`${props.char.name}-3`}>{getHomeWorld(`${props.char.homeworld}`)}</span></li>
+                <li>Speaks: <span id={`${props.char.name}-2`}>{getLang(`${props.char.species[0]}`)}</span></li>
             </ul>
+            {/* <div className="char-img"></div>
+            <div className="char-head">
+                <h3 className="char-name">{props.char.name}</h3>
+                <p>Movies</p>
+            </div>
+            <ul className="film-list hide"> 
+                {props.char.films.map( film => {
+                    return (
+                        <li id={film}>{getFilm(`${film}`)}</li>
+                    )
+                })}
+            </ul> */}
         </div>
     )
 }
