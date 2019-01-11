67d3bc1dbe4e6e2b62ac1e42655bbcf406b3cd60
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-01-11 11:47:22 -0500

    Created character component to map over character list to make character cards for each character.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CharacterCards from './components/CharacterCards'
 import './App.css';
 
 class App extends Component {
@@ -13,11 +14,11 @@ class App extends Component {
     this.getCharacters('https://swapi.co/api/people');
   }
 
-  getCharacters = URL => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
-    fetch(URL)
+  getCharacters = (URL) => {
+
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    
+    fetch(proxyUrl + URL)
       .then(res => {
         return res.json();
       })
@@ -33,6 +34,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <CharacterCards characters={this.state.starwarsChars}/>
       </div>
     );
   }
